d68813623612ac682052baeb0beb429bdca2b2ec
Umut Can <Umut Can@DESKTOP-UIA7P7B>
2021-08-20 10:41:31 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type syfety -- tip güvenliği
+            //kategoriEtiketi burada değer tutucu olarak kullanıldı yani alias
+            string kategoriEtiketi = "kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45; // hata verme sebebi eşittir işareti koymaman.
+            bool sistemeGirisYapmısmi = false;
+
+
+            Console.WriteLine(kategoriEtiketi);
+            Console.WriteLine(ogrenciSayisi);
+            Console.WriteLine(faizOrani);
+            Console.WriteLine(sistemeGirisYapmısmi);
+            Console.ReadLine();//eğer konsolda görünmesini istersen Readline yaz.
+
+        }
+    }
+}
